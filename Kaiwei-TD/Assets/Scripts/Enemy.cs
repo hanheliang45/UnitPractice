249975fdb0882a0490c07e869d11e8321a065e7b
hanheliang45 <hanheliang45@gmail.com>
2023-12-26 22:11:54 -0800

    add move to 1st WayPoint

--- Kaiwei-TD/Assets/Scripts/Enemy.cs
+++ Kaiwei-TD/Assets/Scripts/Enemy.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 4;
+    int targetid = 0;
+    [SerializeField] float moveSpeed;
+    [SerializeField] Transform wayPoints;
+    private List<Transform> WayPointList; 
+    private void Start()
+    {
+        WayPointList = new List<Transform>();
+        foreach (Transform t in wayPoints)
+        { 
+            WayPointList.Add(t);
+        }
 
+    }
     void Update()
     {
-        //this.transform.position += { direction} moveSpeed * Time.deltaTime;
+        this.MoveTo(WayPointList[targetid]);
+        
+    }
+    void MoveTo(Transform target) 
+    {
+        Vector3 targetPosition = target.position;
+        Vector3 MyPosition = this.transform.position;
+        Vector3 direction = (targetPosition - MyPosition).normalized;
+        this.transform.position += direction * moveSpeed * Time.deltaTime;
     }
 }
