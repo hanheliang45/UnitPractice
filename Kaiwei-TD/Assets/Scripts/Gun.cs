a574760580fbcc793f3453328bbe43d4f85190d3
hanheliang45 <hanheliang45@gmail.com>
2023-12-31 17:42:48 -0800

    aim

--- Kaiwei-TD/Assets/Scripts/Gun.cs
+++ Kaiwei-TD/Assets/Scripts/Gun.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField] float range;
+    [SerializeField] LayerMask enemyLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,20 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        this.Aim();    
+    }
+    private void Aim() 
+    {
+        Collider[] colliderList = Physics.OverlapSphere(transform.position, range,enemyLayer);
+        foreach (Collider i in colliderList)
+        {
+            Debug.Log(colliderList.Length);    
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
