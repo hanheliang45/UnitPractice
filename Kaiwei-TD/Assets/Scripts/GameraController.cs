a7bf26af78e2b3ec3050013ff806aaa38c7c9d19
hanheliang45 <hanheliang45@gmail.com>
2024-01-12 22:54:48 -0800

    move camera

--- Kaiwei-TD/Assets/Scripts/GameraController.cs
+++ Kaiwei-TD/Assets/Scripts/GameraController.cs
@@ -13,6 +13,28 @@ public class GameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        this.Move();    
     }
+    private void Move() 
+    {
+        Vector3 MoveDirection = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.UpArrow)) 
+        {
+            MoveDirection += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveDirection += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveDirection += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveDirection += new Vector3(-1, 0, 0);
+        }
+        int MoveSpeed = 20;
+        this.transform.position += MoveDirection * MoveSpeed * Time.deltaTime;
+    } 
 }
