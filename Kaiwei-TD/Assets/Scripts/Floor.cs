784a8cd8f218fa200a66f901b53da164958bd404
hanheliang45 <hanheliang45@gmail.com>
2024-01-20 19:32:57 -0800

    Floor mouse color

---
+++ Kaiwei-TD/Assets/Scripts/Floor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Floor : MonoBehaviour
+{
+    [SerializeField] Material WhenSelected;
+    MeshRenderer meshRenderer;
+    Material normalMaterial;
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        normalMaterial = meshRenderer.material;
+    }
+    private void OnMouseEnter()
+    {
+        meshRenderer.material = WhenSelected;    
+    }
+    private void OnMouseExit() 
+    {
+        meshRenderer.material = normalMaterial;
+    }
+}
